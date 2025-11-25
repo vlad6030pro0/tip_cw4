c2c43c665ec634739c5ce8d736ac97c83898c8ed
N4K <pronin6030@gmail.com>
2025-11-25 17:42:10 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+const dictionary = [
+  {word: "привет", translation: "hello"},
+  {word: "пока", translation: "goodbye"},
+  {word: "пить", translation: "drink"},
+  {word: "прыжок", translation: "jump"},
+  {word: "полёт", translation: "fly"}
+]
+
 function App() {
+  const [index, setIndex] = useState(0);
+  const [trans, showTrans] = useState(false);
+
+  const currentPair = dictionary[index];
+
+  const nextWord = () => {
+    if((index + 1) === dictionary.length){
+      setIndex(0);
+    }else{
+      setIndex(index + 1);
+    }
+
+    showTrans(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Тренажёр перевода слов!</h1>
+      <div>
+        <h3>Слово:</h3>
+        <h4>{currentPair.word}</h4>
+        <h3>Перевод:</h3>
+        {
+          trans ? <h4>{dictionary[index].translation}</h4> : <h4>**********</h4>
+        }
+        <button onClick={() => showTrans(true)}>Нажми, чтобы увидеть перевод!</button>
+        <button style={{margin: '40px'}} onClick={() => nextWord()}>
+          Продолжить
+        </button>
+      </div>
     </div>
   );
 }
